0f4f07b6e7364d4968be97a16ddb6760aba7427b
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2024-10-26 18:36:28 +0530

    bug fix user hashpassword

--- aggregator/crud/crud_user.py
+++ aggregator/crud/crud_user.py
@@ -6,12 +6,12 @@ Refer to generic user schema in callisto which has separate schemas for user cre
 from datetime import datetime
 
 from aggregator.core.db import user_conn
-from aggregator.schemas import User, UserCreate
+from aggregator.schemas import User, UserCreate, UserInDB
 from aggregator.utils.auth import get_password_hash
 
 
 class CRUDUser:
-    def create(self, user: UserCreate) -> User:
+    def create(self, user: UserCreate) -> UserInDB:
         user_data = user.dict()
         user_data["hashedPassword"] = get_password_hash(user.password)
         user_data["createdAt"] = datetime.now()
@@ -20,7 +20,7 @@ class CRUDUser:
         user_data["disabled"] = False
 
         user = User(**user_data)
-        user_conn.insert_user(user)
+        user_conn.insert_user(UserInDB(**user_data))
         return user
 
     def read(self):
