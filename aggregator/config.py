0402db296a082f82b56844cbdffd4ea0e663e6e1
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2024-10-27 09:50:21 +0530

    update access token expiration time

--- aggregator/config.py
+++ aggregator/config.py
@@ -18,7 +18,7 @@ class Config(BaseSettings):
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv(
-        "ACCESS_TOKEN_EXPIRE_MINUTES", 30
+        "ACCESS_TOKEN_EXPIRE_MINUTES", 120
     )
 
 
