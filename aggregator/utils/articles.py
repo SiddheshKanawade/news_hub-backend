bb7bf11570bc440189f18d104a0c30aa5f1994cd
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2024-11-01 23:17:58 +0530

    adding headers to request

--- aggregator/utils/articles.py
+++ aggregator/utils/articles.py
@@ -5,13 +5,32 @@ from aggregator.core import logger
 from aggregator.schemas import Attachment, Author, NewsArticle
 
 
+import requests
+
 def get_general_articles():
-
     articles = []
-    try:
-        response = requests.get(config.GENERAL_FEED_URL)
+    headers = {
+        "authority": "rss.app",
+        "method": "GET",
+        "scheme": "https",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Sec-Ch-Ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        "Sec-Ch-Ua-Mobile": "?1",
+        "Sec-Ch-Ua-Platform": '"Android"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Mobile Safari/537.36"
+    }
 
-        for article in response.json()["items"]:
+    try:
+        response = requests.get(config.GENERAL_FEED_URL, headers=headers)
+        response.raise_for_status()  # Check if the request was successful
+
+        for article in response.json().get("items", []):
             articles.append(
                 NewsArticle(
                     url=article["url"],
@@ -22,20 +41,16 @@ def get_general_articles():
                     datePublished=article["date_published"],
                     source=[
                         Author(name=author["name"])
-                        for author in article["authors"]
-                    ]
-                    if "authors" in article.keys() and article["authors"]
-                    else [],
+                        for author in article.get("authors", [])
+                    ],
                     attachments=[
                         Attachment(url=attachment["url"])
-                        for attachment in article["attachments"]
-                    ]
-                    if "attachements" in article.keys()
-                    and article["attachments"]
-                    else [],
+                        for attachment in article.get("attachments", [])
+                    ],
                 )
             )
         return articles
     except Exception as e:
         logger.error(f"Error fetching general articles: {str(e)}")
         return articles
+
